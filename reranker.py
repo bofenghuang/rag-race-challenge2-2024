89fb49e625f6f994b2e7f95f2c3f96a5c98cac7d
Moussa Kamal Eddine <mkamaleddine@zaion.ai>
2024-02-02 14:09:11 +0000

    Handling if no validated docs

--- reranker.py
+++ reranker.py
@@ -6,6 +6,7 @@ import torch
 
 DEFAULT_TEXT_RERANK_TMPL = "context: {context}\n\nCan you find some useful information in the previous context that can help you answer the following question? \nquestion: {query}.\n\nAnswer only with 'Yes' or 'No', don't say anything else !!"
 
+
 class Reranker:
     def __init__(
         self,
@@ -24,30 +25,56 @@ class Reranker:
                 "Please install both with `pip install transformers[torch]`."
             ) from exc
 
-        self._tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, **tokenizer_kwargs)
+        self._tokenizer = AutoTokenizer.from_pretrained(
+            model_name_or_path, **tokenizer_kwargs
+        )
         self._tokenizer.add_special_tokens({"pad_token": "<s>"})
-        self._model = AutoModelForCausalLM.from_pretrained(model_name_or_path, **model_kwargs).to(device)
+        self._model = AutoModelForCausalLM.from_pretrained(
+            model_name_or_path, **model_kwargs
+        ).to(device)
 
         self._no_id = self._tokenizer.encode("No")[-1]
         self._yes_id = self._tokenizer.encode("Yes")[-1]
 
     def _predict(self, query, contexts):
-        inputs = [DEFAULT_TEXT_RERANK_TMPL.format(query=query, context=context) for context in contexts]
-        inputs_messages = [
-            self._tokenizer.apply_chat_template([{"role": "user", "content": input}], tokenize=False) for input in inputs
+        inputs = [
+            DEFAULT_TEXT_RERANK_TMPL.format(query=query, context=context)
+            for context in contexts
         ]
-        tokens = self._tokenizer(inputs_messages, return_tensors="pt", padding=True).to(self._model.device)
+        inputs_messages = [
+            self._tokenizer.apply_chat_template(
+                [{"role": "user", "content": input}], tokenize=False
+            )
+            for input in inputs
+        ]
+        tokens = self._tokenizer(inputs_messages, return_tensors="pt", padding=True).to(
+            self._model.device
+        )
 
         with torch.no_grad():
             out = self._model(tokens["input_ids"])
 
-        return [(out["logits"][i][-1][self._no_id] < out["logits"][i][-1][self._yes_id]).item() for i in range(out["logits"].shape[0])]
+        return [
+            (
+                out["logits"][i][-1][self._no_id] < out["logits"][i][-1][self._yes_id]
+            ).item()
+            for i in range(out["logits"].shape[0])
+        ]
 
     def __call__(self, query: str, documents: List):
         document_texts = [doc.text for doc in documents]
         is_validated = self._predict(query, document_texts)
 
-        retrieved_documents = [doc for doc, is_validated_ in zip(documents, is_validated) if is_validated_]
-        logging.info(f"{self.__class__} retrieved {len(retrieved_documents)} documents from {len(documents)}")
+        retrieved_documents = [
+            doc for doc, is_validated_ in zip(documents, is_validated) if is_validated_
+        ]
+        if len(retrieved_documents) == 0:
+            logging.info("No validated documents, retrurning top 2 documents")
+            retrieved_documents = sorted(
+                documents, key=lambda x: x.embed_score, reverse=True
+            )[:2]
+        logging.info(
+            f"{self.__class__} retrieved {len(retrieved_documents)} documents from {len(documents)}"
+        )
 
         return retrieved_documents
