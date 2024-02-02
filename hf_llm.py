ad5aa68fc5b7f3a4c47ae029762a7ad48e7665fe
bofenghuang <bofenghuang7@gmail.com>
2024-02-02 12:20:20 +0100

    up

--- hf_llm.py
+++ hf_llm.py
@@ -1,28 +1,84 @@
 # coding=utf-8
 
-class HuggingFaceLLM:
-    def _load_model(
-            self,
-            model_name,
-            context_window,
-            max_new_tokens,
-            system_prompt,
-            ):
+from typing import Optional
+import torch
 
-    try:
-        import torch
-        from transformers import (
-            AutoModelForCausalLM,
-            AutoTokenizer,
-            StoppingCriteria,
-            StoppingCriteriaList,
+
+class HuggingFaceLLM:
+    def __init__(self, **kwargs) -> None:
+        self._load_model(**kwargs)
+
+    def _load_model(
+        self,
+        model_name,
+        # context_window,
+        # max_new_tokens,
+        # system_prompt,
+        device_map: Optional[str] = "auto",
+        model_kwargs: Optional[dict] = None,
+        tokenizer_kwargs: Optional[dict] = None,
+        generate_kwargs: Optional[dict] = None,
+        query_wrapper_prompt: str = "{query}",
+    ):
+
+        try:
+            from transformers import (
+                AutoModelForCausalLM,
+                AutoTokenizer,
+                StoppingCriteria,
+                StoppingCriteriaList,
+            )
+        except ImportError as exc:
+            raise ImportError(
+                f"{type(self).__name__} requires torch and transformers packages.\n"
+                "Please install both with `pip install transformers[torch]`."
+            ) from exc
+
+        self._tokenizer = AutoTokenizer.from_pretrained(model_name, **tokenizer_kwargs)
+        self._model = AutoModelForCausalLM.from_pretrained(model_name, device_map=device_map, **model_kwargs)
+
+        self._generate_kwargs = generate_kwargs
+        self._query_wrapper_prompt = query_wrapper_prompt
+
+    def _predict(self, query):
+        full_prompt = self._query_wrapper_prompt.format(query=query)
+
+        inputs = self._tokenizer(full_prompt, return_tensors="pt")
+        inputs = inputs.to(self._model.device)
+
+        tokens = self._model.generate(
+            **inputs,
+            # max_new_tokens=self.max_new_tokens,
+            # stopping_criteria=self._stopping_criteria,
+            **self._generate_kwargs,
         )
-    except ImportError as exc:
-        raise ImportError(
-            f"{type(self).__name__} requires torch and transformers packages.\n"
-            "Please install both with `pip install transformers[torch]`."
-        ) from exc
-    
-        self._model = model or AutoModelForCausalLM.from_pretrained(
-        model_name, device_map=device_map, **model_kwargs
-    )
+        completion_tokens = tokens[0][inputs["input_ids"].size(1) :]
+        completion = self._tokenizer.decode(completion_tokens, skip_special_tokens=True)
+
+        return completion
+
+
+# llm_model_name_or_path = "mistralai/Mistral-7B-Instruct-v0.2"
+# # llm_model_name_or_path = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+# # llm_model_name_or_path = "microsoft/phi-2"
+
+# query_wrapper_prompt = "<s>[INST] {query} [/INST]"
+# # query_wrapper_prompt = "Instruct: {query}\nOutput: "
+
+# model = HuggingFaceLLM(
+#     model_name=llm_model_name_or_path,
+#     device_map="auto",
+#     # device_map="sequential",
+#     # uncomment this if using CUDA to reduce memory usage
+#     model_kwargs={
+#         "torch_dtype": torch.float16,
+#         # "max_memory": {0: '40GiB', 1: '40GiB', 2: '40GiB'},
+#     },
+#     tokenizer_kwargs={"max_length": 32768},
+#     generate_kwargs={"max_new_tokens": 256, "temperature": 0, "do_sample": False},
+#     query_wrapper_prompt=query_wrapper_prompt,
+# )
+
+# query = "How is content moderation carried out on X?"
+# r = model._predict(query)
+# print(r)
--- run.py
+++ run.py
@@ -8,6 +8,7 @@ os.environ["HF_HOME"] = "/projects/bhuang/.cache/huggingface"
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 os.environ["BITSANDBYTES_NOWELCOME"] = "1"
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,2,3,4,5"
 # os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
 
 import json
@@ -16,12 +17,14 @@ import logging
 import sys
 from collections import defaultdict
 from pathlib import Path
+import torch
 
 
 from reader import NotSimpleDirectoryReader, get_file_metadata
 from category_retriever import ModerateBM25CategoryRetriever
 from embed_retriever import BGEM3EmbedModel, BGEM3EmbedDocumentRetriever
 from simple_reader import SimplerReader
+from hf_llm import HuggingFaceLLM
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
@@ -36,7 +39,7 @@ input_dir = "/home/bhuang/nlp/rag-race-challenge2-2024/platform-docs-versions-sa
 df = pd.read_csv(input_csv_file, sep=";")
 queries = df["question"].tolist()
 
-# # load documents
+# load documents
 
 documents, documents_chunks, documents_smaller_chunks, documents_smaller_smaller_chunks, smallest_chunks = SimplerReader(
     input_dir
@@ -95,7 +98,6 @@ docs = [
 
 doc_names = list(set([d.metadata["platform"] for d in docs]))
 
-# todo: nbest
 category_document_retriever = ModerateBM25CategoryRetriever(
     doc_names,
     name_mapping={"x": "x twitter tweet", "booking.com": "booking", "facebook": "facebook meta"},  # todo
@@ -103,7 +105,7 @@ category_document_retriever = ModerateBM25CategoryRetriever(
 )
 
 # Setting use_fp16 to True speeds up computation with a slight performance degradation
-embed_model = BGEM3EmbedModel("BAAI/bge-m3", use_fp16=True, device=0)
+embed_model = BGEM3EmbedModel("BAAI/bge-m3", use_fp16=True, device=3)
 embed_document_retriever = BGEM3EmbedDocumentRetriever(
     embed_model,
     batch_size=12,
@@ -112,7 +114,7 @@ embed_document_retriever = BGEM3EmbedDocumentRetriever(
     # score_name="dense_score",
     # score_name = "sparse_dense_score",
     score_name="colbert_sparse_dense_score",
-    # weights_for_different_modes = None,
+    # weights_for_different_modes = None,  # todo: 
     top_k=5,
 )
 
@@ -133,3 +135,41 @@ print(docs_)
 # data = [{"query": query, "catgory": category_document_retriever(query, docs)[1]} for query in queries]
 # df = pd.DataFrame(data)
 # df.to_json("./tmp_query_category.json", orient="records", force_ascii=False)
+
+DEFAULT_TEXT_QA_PROMPT_TMPL = (
+    "Context information is below.\n"
+    "---------------------\n"
+    "{context}\n"
+    "---------------------\n"
+    "Given the context information and not prior knowledge, "
+    "answer the query.\n"
+    "Query: {query}\n"
+    "Answer: "
+)
+
+# llm_model_name_or_path = "mistralai/Mistral-7B-Instruct-v0.2"
+llm_model_name_or_path = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+# llm_model_name_or_path = "microsoft/phi-2"
+
+query_wrapper_prompt = "<s>[INST] {query} [/INST]"
+# query_wrapper_prompt = "Instruct: {query}\nOutput: "
+
+model = HuggingFaceLLM(
+    model_name=llm_model_name_or_path,
+    # device_map="auto",
+    device_map="sequential",
+    # uncomment this if using CUDA to reduce memory usage
+    model_kwargs={
+        "torch_dtype": torch.float16,
+        "max_memory": {0: '40GiB', 1: '40GiB', 2: '40GiB'},
+    },
+    tokenizer_kwargs={"max_length": 32768},
+    generate_kwargs={"max_new_tokens": 256, "temperature": 0, "do_sample": False, "prompt_lookup_num_tokens": 10},
+    query_wrapper_prompt=query_wrapper_prompt,
+)
+
+context = "\n\n".join([doc.text for doc in docs_])
+context_query = DEFAULT_TEXT_QA_PROMPT_TMPL.format(context=context, query=query)
+
+r = model._predict(context_query)
+print(r)
