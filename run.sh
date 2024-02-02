e1b7b40a24afc5c19c263c69038ea402322ac9e9
bofenghuang <bofenghuang7@gmail.com>
2024-02-02 17:33:11 +0100

    up

--- run.sh
+++ run.sh
@@ -28,8 +28,9 @@ export HF_HOME="$ALL_CCFRWORK/.cache/huggingface"
 # models
 embed_model_name_or_path="BAAI/bge-m3"
 reranker_model_name_or_path="mistralai/Mistral-7B-Instruct-v0.2"
+llm_model_name_or_path="mistralai/Mistral-7B-Instruct-v0.2"
 # llm_model_name_or_path="/gpfsdswork/dataset/HuggingFace_Models/TheBloke/Mixtral-8x7B-Instruct-v0.1-AWQ"
-llm_model_name_or_path="/gpfsdswork/dataset/HuggingFace_Models/mistralai/Mixtral-8x7B-Instruct-v0.1"
+# llm_model_name_or_path="/gpfsdswork/dataset/HuggingFace_Models/mistralai/Mixtral-8x7B-Instruct-v0.1"
 
 # data
 input_csv_file="./challenge-2-dataset-and-documentation/dataset/train/input/questions.csv"
